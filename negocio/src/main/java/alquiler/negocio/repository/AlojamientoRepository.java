482e252651ab21f635081b6aa1d7d4d2a0489b5f
Pablo Alvarez <pablo.alvarez8@alu.uclm.es>
2025-10-05 17:38:18 +0200

    Revert "Creada entity y repository de alojamiento y la subtabla de disponiblidad"
    This reverts commit a1ad421f32c1e905aa95f89abf01c43d03132fd7.

--- negocio/src/main/java/alquiler/negocio/repository/AlojamientoRepository.java
+++ negocio/src/main/java/alquiler/negocio/repository/AlojamientoRepository.java
@@ -1,8 +1,5 @@
 package alquiler.negocio.repository;
 
-
-import alquiler.negocio.entity.Alojamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlojamientoRepository extends JpaRepository<Alojamiento, Long> {
+public class AlojamientoRepository {
+    
 }
