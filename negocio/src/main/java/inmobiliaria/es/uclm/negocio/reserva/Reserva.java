718c4f2380b43108adf3e07aea3256021ab2f8bf
Pablo Alvarez <pablo.alvarez8@alu.uclm.es>
2025-12-08 20:06:18 +0100

    Solucionamos algunos errores

--- negocio/src/main/java/inmobiliaria/es/uclm/negocio/reserva/Reserva.java
+++ negocio/src/main/java/inmobiliaria/es/uclm/negocio/reserva/Reserva.java
@@ -3,12 +3,18 @@ package inmobiliaria.es.uclm.negocio.reserva;
 import inmobiliaria.es.uclm.negocio.alojamiento.Alojamiento;
 import inmobiliaria.es.uclm.negocio.user.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "reserva") // <--- Recomendado: Nombre explícito en minúsculas
 public class Reserva {
 
+    // Getters y Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,19 +37,6 @@ public class Reserva {
     
     // Derby no tiene ENUM, así que lo dejamos como String. 
     // Valores esperados: "PENDIENTE", "ACEPTADA", "RECHAZADA"
-    private String estado; 
+    private String estado;
 
-    // Getters y Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public User getInquilino() { return inquilino; }
-    public void setInquilino(User inquilino) { this.inquilino = inquilino; }
-    public Alojamiento getAlojamiento() { return alojamiento; }
-    public void setAlojamiento(Alojamiento alojamiento) { this.alojamiento = alojamiento; }
-    public LocalDate getFechaEntrada() { return fechaEntrada; }
-    public void setFechaEntrada(LocalDate fechaEntrada) { this.fechaEntrada = fechaEntrada; }
-    public LocalDate getFechaSalida() { return fechaSalida; }
-    public void setFechaSalida(LocalDate fechaSalida) { this.fechaSalida = fechaSalida; }
-    public String getEstado() { return estado; }
-    public void setEstado(String estado) { this.estado = estado; }
 }
--- negocio/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- TABLA USUARIO (Singular, coincidiendo con @Table(name="usuario"))
-INSERT INTO usuario (nombre, apellido, correo, contrasena, role, created_at) 
-VALUES ('Pablo', 'Test', 'admin@test.com', '1234', 'INQUILINO', CURRENT_TIMESTAMP);
