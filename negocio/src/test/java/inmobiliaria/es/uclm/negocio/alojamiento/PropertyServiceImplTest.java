c8398b07f07ce331955116643a8a8a801a43ecfa
Pablo Alvarez <pablo.alvarez8@alu.uclm.es>
2025-12-21 14:29:30 +0100

    Cambiamos el nombre de PropertyServiceTest a PropertyServiceImplTest.

--- negocio/src/test/java/inmobiliaria/es/uclm/negocio/alojamiento/PropertyServiceImplTest.java
+++ negocio/src/test/java/inmobiliaria/es/uclm/negocio/alojamiento/PropertyServiceImplTest.java
@@ -24,7 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class PropertyServiceTest {
+class PropertyServiceImplTest {
 
     @Mock
     private AlojamientoRepository propertyRepo;
