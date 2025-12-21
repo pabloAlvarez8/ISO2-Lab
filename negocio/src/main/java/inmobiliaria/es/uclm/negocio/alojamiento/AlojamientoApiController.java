c5cab228808cda5c506e8f83a8afb43d0067aaa2
Daniel López Rodríguez <Daniel.Lopez48@alu.uclm.es>
2025-12-21 16:18:26 +0100

    Replaced usage of 'Stream.collect(Collectors.toList())' with 'Stream.toList()'

--- negocio/src/main/java/inmobiliaria/es/uclm/negocio/alojamiento/AlojamientoApiController.java
+++ negocio/src/main/java/inmobiliaria/es/uclm/negocio/alojamiento/AlojamientoApiController.java
@@ -47,6 +47,6 @@ public class AlojamientoApiController {
 
                 return alojamientosEncontrados.stream()
                                 .map(AlojamientoSearchResultDTO::fromEntity)
-                                .collect(Collectors.toList());
+                                .toList();
         }
 }
